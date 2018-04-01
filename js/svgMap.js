2a1c9adbbe656e39af328b7f64390ed46e3a3476
fan <sadf0707@gmail.com>
2018-04-01 23:16:08 +0800

    first commit

---
+++ js/svgMap.js
@@ -0,0 +1,114 @@
+/*
+target
+url
+center
+zoom
+// TODO
+px percentage drag event problem
+*/
+/*
+setmarker(tooltip)(infowindow)
+setpolyline
+setpolygan
+setchart
+setcontextmenu
+removemarker
+zoomto
+panto
+*/
+'use strict';
+(function(root, factory) {
+	root.svgMap = factory(root);
+}(typeof window !== 'undefined' ? window : this, function(win) {
+    let svgMap = (function(option) {
+		let me=this;
+		me.option=option;
+		let param={
+			'targer':document.querySelector('#'+me.option.target),
+			'map' :document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
+			'zoomLayer' :document.createElementNS('http://www.w3.org/2000/svg', 'g'),
+			'markerLayer' :document.createElementNS('http://www.w3.org/2000/svg', 'g'),
+			'startX':0,
+			'startY':0,
+			'translateX':0,
+			'translateY':0,
+			'scale' : 1,
+			'translateXt' :0,
+			'translateYt' :0,
+			'scaleUnit':.5
+		};
+		let mouseup=function(event){
+			me.zoomEventOff();
+			param.translateX=(param.startX-event.clientX+param.translateX);
+			param.translateY=(param.startY-event.clientY+param.translateY);
+		}
+		let mouseout=function(event){
+			me.zoomEventOff();
+			param.translateX=(param.startX-event.clientX+param.translateX);
+			param.translateY=(param.startY-event.clientY+param.translateY);
+		}
+		let mousemove=function(event){
+			param.translateXt=(param.startX-event.clientX+param.translateX);
+			param.translateYt=(param.startY-event.clientY+param.translateY);
+			param.zoomLayer.setAttribute('transform','translate('+param.translateXt+','+param.translateYt+') scale('+param.scale+')');
+		}
+    	this.init= function(){
+			me.initImage();
+			me.initZoomEvent();
+    	}
+		this.initImage=function(){
+			let image = document.createElementNS('http://www.w3.org/2000/svg','image');
+			param.map.setAttribute('viewBox', '0 0 '+param.targer.clientWidth+' '+param.targer.clientHeight);
+			param.zoomLayer.setAttribute('transform', 'translate(0,0) scale(1)');
+			image.setAttributeNS("http://www.w3.org/1999/xlink", 'xlink:href', me.option.url);
+			image.style.width=param.targer.clientWidth;
+			image.style.height=param.targer.clientHeight;
+			param.zoomLayer.appendChild(image);
+			param.map.appendChild(param.zoomLayer);
+			param.map.appendChild(param.markerLayer);
+			param.targer.appendChild(param.map);
+		}
+		this.initZoomEvent=function(){
+			me.zoomEventOn();
+		}
+		this.setMarker=function(markerOpt){
+			let image = document.createElementNS('http://www.w3.org/2000/svg','image');
+			let imageParent = document.createElementNS('http://www.w3.org/2000/svg','g');
+			image.setAttributeNS("http://www.w3.org/1999/xlink", 'xlink:href', markerOpt.icon);
+			image.style.width=markerOpt.width;
+			image.style.height=markerOpt.height;
+			markerOpt.infoWindow;
+			imageParent.appendChild(image);
+			imageParent.setAttribute('transform', 'translate('+markerOpt.x+','+markerOpt.y+') scale(1)');
+			param.markerLayer.appendChild(imageParent);
+		}
+		svgMap.prototype.zoomEventOn = function () {
+			param.map.addEventListener("mousedown", function(event){
+				param.startX=event.clientX;
+				param.startY=event.clientY;
+				param.map.addEventListener("mouseleave", mouseout);
+				param.map.addEventListener("mouseup", mouseup);
+				param.map.addEventListener("mousemove", mousemove);
+			});
+			param.map.addEventListener("wheel", function(event){
+				event.preventDefault();
+				if(event.deltaY>0){
+					if(param.scale-param.scaleUnit<=0){
+						return;
+					}
+					param.scale-=param.scaleUnit;
+				} else{
+					param.scale+=param.scaleUnit;
+				}
+				param.zoomLayer.setAttribute('transform','translate('+param.translateXt+','+param.translateYt+') scale('+param.scale+')');
+			});
+		}
+		svgMap.prototype.zoomEventOff = function () {
+			param.map.removeEventListener("mouseleave", mouseout);
+			param.map.removeEventListener("mouseup", mouseup);
+			param.map.removeEventListener("mousemove",  mousemove);
+		}
+		me.init();
+	});
+    return svgMap;
+}));
